d991a5965f3e03693445ffc9d5048247027ea384
María Vedia <mvtercero85@gmail.com>
2019-05-17 14:05:07 +0200

    add background styles to slide component

--- components/carousel-slide.js
+++ components/carousel-slide.js
@@ -1,8 +1,8 @@
 import { LitElement, html, css } from "lit-element";
 
+//TO-DO: Preguntar cómo hacer el background-image: url(${this.image})
 
 class CarouselSlide extends LitElement {
-
   static get properties() {
     return {
       image: { type: String },
@@ -11,15 +11,20 @@ class CarouselSlide extends LitElement {
   }
 
   static get styles() {
-
     return css`
     :host {
       display: block;
+     
     }
     .slide {
       width: 100%;
       height: 100%;
       border: 1px solid blue;
+    }
+    .image-position {
+      background-size: cover,
+      background-repeat: no-repeat,
+      background-position: 50% 60%
     }
     `;
   }
@@ -30,7 +35,7 @@ class CarouselSlide extends LitElement {
 
   render() {
     return html`
-    <li class="slide">
+    <li class="slide" class="image-position">
       <img src=${this.image} alt="">
     </li>
   `
